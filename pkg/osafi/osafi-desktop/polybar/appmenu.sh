a092a9ade2f98cc9dfd7752713fda51bf77a2fca
Omeed Safi <omeed@safi.ms>
2019-02-15 22:23:17 -0800

    Work on polybar

---
+++ pkg/osafi/osafi-desktop/polybar/appmenu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rofi -modi run,drun \
+	-columns 1 \
+	-drun-icon-theme "Papirus" \
+	-font "Fantasque Sans Mono 10" \
+	-hide-scrollbar \
+	-line-padding 4 \
+	-lines 10 \
+	-location 1 \
+	-padding 25 \
+	-show drun \
+	-show-icons \
+	-width 25 \
+	-xoffset 28 \
+	-yoffset 60
---
+++ pkg/osafi/osafi-desktop/polybar/appmenu_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rofi -modi run,drun,window \
+	-columns 3 \
+	-font "Fantasque Sans Mono 10" \
+	-lines 10 \
+	-location 1 \
+	-padding 20 \
+	-show drun \
+	-sidebar-mode \
+	-width 50  \
+	-xoffset 28 \
+	-yoffset 60
