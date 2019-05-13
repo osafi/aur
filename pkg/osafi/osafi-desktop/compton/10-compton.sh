295684d84537f8947b0817987606ddbfb7663137
Omeed Safi <omeed@safi.ms>
2019-05-12 22:50:02 -0700

    Remove shadows from menus

--- pkg/osafi/osafi-desktop/compton/10-compton.sh
+++ pkg/osafi/osafi-desktop/compton/10-compton.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# Terminate already running compton
+killall -q compton
+while pgrep -u $UID -x compton >/dev/null; do sleep 1; done
+
 compton --config /etc/osafi/compton/compton.conf &
